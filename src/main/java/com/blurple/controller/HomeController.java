a38d3cd75c30a7be54dff97b5333a7bd9e7f30c1
kevin <kevin.li.3@stonybrook.edu>
2017-03-31 19:40:07 -0400

    changed user to qauser

--- src/main/java/com/blurple/controller/HomeController.java
+++ src/main/java/com/blurple/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.blurple.controller;
 
+import com.blurple.models.QAUser;
+import com.blurple.models.Course;
+
 import java.util.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.google.appengine.api.datastore.DatastoreService;
@@ -21,3 +26,34 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.googlecode.objectify.ObjectifyService;
+
+@Controller
+@SessionAttributes("sess")
+public class HomeController {
+
+  @RequestMapping("/landing")
+  public ModelAndView loadLandingPage() {
+    ModelAndView mv = new ModelAndView("landing");
+    return mv;
+  }
+
+  @RequestMapping("/home")
+  public ModelAndView loadHomePage() {
+    // add params and checks for login. otherwise, redirect back to landing page
+    // create a session upon login
+    ModelAndView mv = new ModelAndView("home");
+
+    String userId = "kevin";
+    QAUser userObj = ObjectifyService.ofy().load().type(QAUser.class).id(userId).now();
+
+    // load up all user information
+    // might need to parse to json
+    if (userObj != null) {
+      Set<Course> courses = userObj.getCourses();
+      mv.addObject("enrolledCourses", courses);
+    }
+
+    return mv;
+
+  }
+}
--- src/main/java/com/blurple/models/QAUser.java
+++ src/main/java/com/blurple/models/QAUser.java
@@ -11,25 +11,30 @@ import com.googlecode.objectify.annotation.Parent;
 import java.lang.String;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 
 @Entity
-public class User {
+public class QAUser {
   @Id public Long id;
 
   public String firstName;
   public String lastName;
   public boolean isProfessor;
-  public List<Course> courses;
+  public Set<Course> courses;
   public List<Post> posts;
 
 
-  public User() {
+  public QAUser() {
     
   }
 
   public Long getId() {
     return id;
+  }
+
+  public Set<Course> getCourses() {
+    return courses;
   }
 
 }
